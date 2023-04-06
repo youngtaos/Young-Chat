3d7dfe1ce5d90ec20137c885ba093a2c8122b035
youtous <1283374626@qq.com>
2023-04-06 20:42:16 +0800

    加入自己的logo

--- src/pages/me/index.jsx
+++ src/pages/me/index.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import MeCollecting from "./components/MeCollecting";
 import MeInfo from "./components/MeInfo";
-import { login } from "./utils";
 import Login from "./components/Login";
+import styles from "./styles.module.scss";
 
 const Index = () => {
   const [userId, setUserId] = useState("");
   return (
-    <>
+    <view className={styles.wrapper}>
       {userId ? (
         <MeInfo userId={userId}></MeInfo>
       ) : (
         <Login setUserId={setUserId} userId={userId} />
       )}
       <MeCollecting userId={userId} />
-    </>
+      <view className={styles.bottomBox}></view>
+    </view>
   );
 };
 
