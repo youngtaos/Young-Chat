206d13db85ab427e95b42d648bad0587e5f53681
youtous <1283374626@qq.com>
2023-04-08 23:23:25 +0800

    准备开始写问题的详情页

---
+++ src/pages/index/components/QuestionDetail/index.jsx
@@ -0,0 +1,8 @@
+import { View } from "@tarojs/components";
+import styles from "./styles.module.scss";
+
+const QuestionDetail = () => {
+  return <View className={styles.wrapper}>detail</View>;
+};
+
+export default QuestionDetail;
